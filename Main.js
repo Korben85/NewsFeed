dac26d969d32d9e7fd5faaffeca0f24fbf5c3fb8
Korben85 <Korben85@users.noreply.github.com>
2020-09-18 14:06:52 +0200

    first commit

---
+++ Main.js
@@ -0,0 +1,39 @@
+"use strict;";
+
+console.log("Start Feed reading ...");
+
+var https = require("follow-redirects").https;
+var fs = require("fs");
+
+var options = {
+  method: "GET",
+  hostname: "mein-mmo.de",
+  path: "/feed/",
+  headers: {},
+  maxRedirects: 20,
+};
+
+var req = https.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    //console.log(body.toString());
+    let para = document.createElement("P"); // Create a <p> element
+    let t = document.createTextNode(body.toString());
+    para.appendChild(t);
+    document.body.appendChild(para);
+  });
+
+  res.on("error", function (error) {
+    console.error(error);
+  });
+});
+
+req.end();
+
+console.log("Finished Feed reading ...");

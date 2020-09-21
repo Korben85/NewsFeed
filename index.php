cf04a279a92b023bd5fd206d851620d977199599
Korben85 <Korben85@users.noreply.github.com>
2020-09-21 12:33:49 +0200

    rewritten in php

--- index.php
+++ index.php
@@ -3,39 +3,21 @@
 <html>
 
 <head>
-    <script type="module" src="bundle.js"></script>
+    <!-- HTML meta URL redirect-->
+    <?php
+    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]/feed.xml";
+    ?>
+    <meta http-equiv="refresh" content="1; url=<?php echo $actual_link ?>">
+
 </head>
 
 <body>
     <?php
-$file = fopen("feed.xml","w");
-echo fwrite($file,"Hello World. Testing!");
-fclose($file);
-?>
-
-    <?php
-require_once 'HTTP/Request2.php';
-$request = new HTTP_Request2();
-$request->setUrl('https://mein-mmo.de/feed/');
-$request->setMethod(HTTP_Request2::METHOD_GET);
-$request->setConfig(array(
-  'follow_redirects' => TRUE
-));
-try {
-  $response = $request->send();
-  if ($response->getStatus() == 200) {
-    echo $response->getBody();
-  }
-  else {
-    echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
-    $response->getReasonPhrase();
-  }
-}
-catch(HTTP_Request2_Exception $e) {
-  echo 'Error: ' . $e->getMessage();
-}
-?>
-
+      $file = fopen("feed.xml","w");
+      fwrite($file,$response = file_get_contents("http://mein-mmo.de/feed/"));
+      fclose($file);
+      // echo $response;
+    ?>
 </body>
 
 </html>
